04f7fc96cdf1519d9d6e9418c141928119448647
Todd Bello <toddbell@microsoft.com>
2018-07-25 14:37:46 -0700

    excess UI and PlayFab calls
    added text to button prefab (doesn't get displayed). added a panel for an inventory screen (doesn't have any other UI around it yet).

--- Meme On/Assets/ManagerLogic.cs
+++ Meme On/Assets/ManagerLogic.cs
@@ -1,10 +1,11 @@
-﻿using System;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using PlayFab;
 using PlayFab.ClientModels;
+using PlayFab.ServerModels;
 
 public class ManagerLogic : MonoBehaviour
 {
@@ -13,50 +14,138 @@ public class ManagerLogic : MonoBehaviour
 
     public Text UserName;
 
+    private string playerPlayFabId = "";
+
     // Use this for initialization
     void Start()
     {
-
         // set up playfab username log in here
         initPlayFab();
     }
 
     void initPlayFab()
     {
-        // Get the text that the user entered
-        //UserName.text;
-        Debug.Log("user name" + UserName.text);
-
         string user = UserName.text;
         if (user == null || user == "")
         {
             user = "DefaultUser";
         }
 
-        var request = new LoginWithCustomIDRequest { CustomId = UserName.text, CreateAccount = true };
+        var request = new LoginWithCustomIDRequest { CustomId = user, CreateAccount = true };
         PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
-
     }
 
     private void OnLoginSuccess(LoginResult result)
     {
-        Debug.Log("Congratulations, you made your first successful API call!");
+        playerPlayFabId = result.PlayFabId;
 
+        setupMemePrinter();
+    }
+
+    private void setupMemePrinter()
+    {
         GameObject body = GameObject.CreatePrimitive(PrimitiveType.Cube);
-        //body.transform.position = new Vector3(0, 0.5f, 0);
         body.transform.position = new Vector3(0, 0, 0);
-        //body.transform.position = new Vector3(0, 0, 0);
 
-        //sphereButton = Instantiate(sphereButton, transform.position, transform.rotation);
         sphereButton = Instantiate(SphereButtonPrefab);
-
-        //buttonPush.collider.
         sphereButton.transform.position = new Vector3(0, 0, -0.5f);
+    }
+
+    public void GetMeme()
+    {
+        if (playerPlayFabId != "")
+        {
+            ExecuteCloudScriptRequest request = new ExecuteCloudScriptRequest();
+            request.FunctionName = "GetMeme";
+            request.SpecificRevision = 11;
+
+            //PlayFabClientAPI.ExecuteCloudScript(request, );
+            PlayFabServerAPI.UpdateUserInternalData(new UpdateUserInternalDataRequest()
+            {
+                PlayFabId = playerPlayFabId,
+                Data = new Dictionary<string, string>() {
+                    // TODO: add value to an "inventory" OR this may just be a kvp, not an expanding table?
+                    // May need to GET the current inventory value first before adding the next value
+                    //{"Clicks", "0"},
+                    //{"InventoryValue", addValue.ToString() }
+            },
+            },
+            result => Debug.Log("Set internal user data successful"),
+            error =>
+            {
+                Debug.Log("Got error updating internal user data:");
+                Debug.Log(error.GenerateErrorReport());
+            });
+        }
+    }
+
+    public void UpdateUserInternalInventoryValue(double addValue)
+    {
+        if (playerPlayFabId != "")
+        {
+            PlayFabServerAPI.UpdateUserInternalData(new UpdateUserInternalDataRequest()
+            {
+                PlayFabId = playerPlayFabId,
+                Data = new Dictionary<string, string>() {
+                    // TODO: add value to an "inventory" OR this may just be a kvp, not an expanding table?
+                    // May need to GET the current inventory value first before adding the next value
+                    //{"Clicks", "0"},
+                    {"InventoryValue", addValue.ToString() }
+            },
+            },
+            result => Debug.Log("Set internal user data successful"),
+            error =>
+            {
+                Debug.Log("Got error updating internal user data:");
+                Debug.Log(error.GenerateErrorReport());
+            });
+        }
+    }
+
+    public void UpdateUserInternalClicks()
+    {
+        if (playerPlayFabId != "")
+        {
+            PlayFabServerAPI.UpdateUserInternalData(new UpdateUserInternalDataRequest()
+            {
+                PlayFabId = playerPlayFabId,
+                Data = new Dictionary<string, string>() {
+                {"Clicks", "0"},
+            },
+            },
+            result => Debug.Log("Set internal user data successful"),
+            error =>
+            {
+                Debug.Log("Got error updating internal user data:");
+                Debug.Log(error.GenerateErrorReport());
+            });
+        }
+    }
+
+    public void GetUserInternalData()
+    {
+        if (playerPlayFabId != "")
+        {
+            PlayFabServerAPI.GetUserInternalData(new PlayFab.ServerModels.GetUserDataRequest()
+            {
+                PlayFabId = playerPlayFabId,
+            },
+            result =>
+            {
+                if (result.Data == null || !result.Data.ContainsKey("Clicks")) Debug.Log("No Clicks");
+                else Debug.Log("Clicks: " + result.Data["Clicks"].Value);
+            },
+            error =>
+            {
+                Debug.Log("Got error getting internal user data:");
+                Debug.Log(error.GenerateErrorReport());
+            });
+        }
     }
 
     private void OnLoginFailure(PlayFabError error)
     {
-        Debug.LogWarning("Something went wrong with your first API call.  :(");
+        Debug.LogWarning("Something went wrong");
         Debug.LogError("Here's some debug information:");
         Debug.LogError(error.GenerateErrorReport());
     }
